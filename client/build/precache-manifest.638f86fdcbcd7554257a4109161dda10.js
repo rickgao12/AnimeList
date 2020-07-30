005095bbb34704bf1913d38ba9d115b6032c929f
dalongerlong 1 <rgao3736@gmail.com>
2020-07-29 20:16:35 -0500

    build? test

---
+++ client/build/precache-manifest.638f86fdcbcd7554257a4109161dda10.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bdba34e8283e0f65ce1b19262877a996",
+    "url": "/index.html"
+  },
+  {
+    "revision": "15bb11311d5ec73bc82e",
+    "url": "/static/css/2.36020323.chunk.css"
+  },
+  {
+    "revision": "85dfff0b8fe4cfa20c82",
+    "url": "/static/css/main.50cfdc09.chunk.css"
+  },
+  {
+    "revision": "15bb11311d5ec73bc82e",
+    "url": "/static/js/2.37359550.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.37359550.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85dfff0b8fe4cfa20c82",
+    "url": "/static/js/main.279fb567.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
