f2953d0afc8977a19d109b4cf226f55519668455
dalongerlong 1 <rgao3736@gmail.com>
2020-07-29 20:53:34 -0500

    file restructure

--- server/index.js
+++ server/index.js
@@ -26,15 +26,13 @@ require('./routes/authRoutes')(app);
 require('./routes/favoriteRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static(path.join(__dirname, 'client/build')));
+	app.use(express.static('client/build'));
 	const path = require('path');
 	app.get('*', (req, res) => {
-		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
 }
-app.get('*', (req, res) => {
-	res.sendFile(path.join(__dirname+'/client/public/index.html'));
-  })
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
