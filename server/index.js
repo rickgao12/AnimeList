826b8939d1db9da51777526f16478a1d69592de2
dalongerlong 1 <rgao3736@gmail.com>
2020-07-29 20:11:02 -0500

    heroku deploy changes

--- server/index.js
+++ server/index.js
@@ -26,13 +26,15 @@ require('./routes/authRoutes')(app);
 require('./routes/favoriteRoutes')(app);
 
 if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('../client/build'));
+	app.use(express.static(path.join(__dirname, 'client/build')));
 	const path = require('path');
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+		res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
 	});
 }
-
+app.get('*', (req, res) => {
+	res.sendFile(path.join(__dirname+'/client/public/index.html'));
+  })
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
